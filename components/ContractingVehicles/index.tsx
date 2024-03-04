b20ffd5ab1eace8b0c2a98507c0eb2198f5b1ce5
yashbais <yash.bais@karoscan.com>
2024-03-04 22:33:36 +0530

    fix updated

--- components/ContractingVehicles/index.tsx
+++ components/ContractingVehicles/index.tsx
@@ -38,8 +38,7 @@ const SidebarLink = () => {
                   </svg>
                 </div>{" "}
                 <div>
-                  541614 - Process, Physical Distribution & Logistics Consulting
-                  Services
+                
                 </div>
               </li> <li className="flex items-center gap-3 ">
                 <div>
@@ -58,18 +57,56 @@ const SidebarLink = () => {
                   </svg>
                 </div>{" "}
                 <div>
-                  541614 - Process, Physical Distribution & Logistics Consulting
-                  Services
+                 
                 </div>
               </li>
-         
+            
+              <li className="flex items-center gap-3 ">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1.5em"
+                    height="1.5em"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="rgb(108 133 17)"
+                      fill-rule="evenodd"
+                      d="M12 21a9 9 0 1 0 0-18a9 9 0 0 0 0 18m-.232-5.36l5-6l-1.536-1.28l-4.3 5.159l-2.225-2.226l-1.414 1.414l3 3l.774.774z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>{" "}
+                <div>
+                 
+                </div>
+              </li>  <li className="flex items-center gap-3 ">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1.5em"
+                    height="1.5em"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="rgb(108 133 17)"
+                      fill-rule="evenodd"
+                      d="M12 21a9 9 0 1 0 0-18a9 9 0 0 0 0 18m-.232-5.36l5-6l-1.536-1.28l-4.3 5.159l-2.225-2.226l-1.414 1.414l3 3l.774.774z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>{" "}
+                <div>
+                 
+                </div>
+              </li>
            
          
             </ul>
           </div>
         </div>
         <div className="flex w-full lg:w-[50%] justify-center lg:justify-start">
-          <div className="aboutBox w-[80%] rounded-[15px] bg-white p-[50px] text-white shadow-xl"
+          <div className="aboutBox w-[80%] rounded-[15px] bg-white px-[50px] py-[30px] text-white shadow-xl"
             style={{ border: "2px solid green", borderRadius: "15px" }}>
 
             <h2 className="decoration-[rgb(235 225 123)] text-[28px] tracking-[1px] underline decoration-2 underline-offset-[5px]"
@@ -85,10 +122,28 @@ const SidebarLink = () => {
                   <div className="flex gap-2">
                     <div> - </div>
                     <div className="text-[21px] text-black ">
-                      U.S. Patent & Trademark
+                    
                     </div>
                   </div>
-                  <div className="pl-[20px] ">Facilities Support Services</div>
+                  <div className="pl-[20px] "></div>
+                </li>
+                <li className=" items-center gap-3 text-black  ">
+                  <div className="flex gap-2">
+                    <div> - </div>
+                    <div className="text-[21px] text-black ">
+                    
+                    </div>
+                  </div>
+                  <div className="pl-[20px] "></div>
+                </li>
+                <li className=" items-center gap-3 text-black  ">
+                  <div className="flex gap-2">
+                    <div> - </div>
+                    <div className="text-[21px] text-black ">
+                    
+                    </div>
+                  </div>
+                  <div className="pl-[20px] "></div>
                 </li>
               
               
